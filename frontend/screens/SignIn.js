32f6d7e66c2783ab9733c41cf69bfc254cdb8ba8
kimhyeongjun95 <hjthelegend@naver.com>
2022-06-02 17:27:49 +0900

    F02-2 로그인 codes refactoring

--- frontend/screens/SignIn.js
+++ frontend/screens/SignIn.js
@@ -86,34 +86,27 @@ const SignIn = ({ navigation }) => {
 
   const naverLoginProcess = async () => {
     try {
-      const token = await naverLogin(initials)
+      const token = await naverLogin(initials);
       const id = await getUserProfile(token.accessToken);
       // 네이버 로그인 실패
       if (id == undefined) {
         console.log("네이버 로그인 실패");
-        return
+        return;
       }
-      API.post('/users/login', {
-        "authId": id
-      })
-      .then((response) => {
-        const result = response.data.profile;
-        if (result === null) {
-          // 아이디가 유효하면서 정보가 없다면?
-          // 반환되는게 null이면 회원가입으로 redirect
-          navigation.navigate('SignUp', {id : id});
-        } 
-        else {
-          // 반환 success면 Home으로 가기
-          setUserId(result.id);
-          setUserNickName(result.nickName);
-          setUserArea(result.area.name)
-          navigation.navigate('Tabs');
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      })
+      const response = await API.post('/users/login', { "authId": id })
+      const result = response.data.profile;
+      if (result === null) {
+        // 아이디가 유효하면서 정보가 없다면?
+        // 반환되는게 null이면 회원가입으로 redirect
+        navigation.navigate('SignUp', {id : id});
+        return;
+      }
+      // 반환 success면 Home으로 가기
+      setUserId(result.id);
+      setUserNickName(result.nickName);
+      setUserArea(result.area.name)
+      navigation.navigate('Tabs');
+      return;
     }
     catch(e)  {
     }
